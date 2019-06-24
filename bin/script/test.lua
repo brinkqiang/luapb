efc62666434944a38ebf36cadd255702cf7cd90d
brinkqiang <brink.qiang@gmail.com>
2019-06-24 11:48:49 +0800

    add

--- bin/script/test.lua
+++ bin/script/test.lua
@@ -3,7 +3,7 @@
 local pb = require("luapb")
 pb.import("net.proto")
 
-function tbcreate()
+function pb_table_create()
     local msg = pb.new("net.tb_Person")
     msg.number = "13615632545"
     msg.email = "13615632545@163.com"
@@ -30,8 +30,8 @@ function pbtest()
 
     print("pbtest " .. binstr)
 end
-
-function pbtest2()
+ 
+function pb_cpp_test()
     local msg = pb.new("net.tb_Person")
     msg.number = "13615632545"
     msg.email = "13615632545@163.com"
@@ -41,14 +41,20 @@ function pbtest2()
     msg.desc:add("second")
     msg.desc:add("three")
 
-    for i = 1,1000000 do
-        local binstr = pb.serializeToString(msg)
-        local msg2 = pb.new("net.tb_Person")
-        pb.parseFromString(msg2, binstr)
+    msg.desc:set(1, "three")
+    msg.desc:set(3, "first")
+    for i = 1, msg.desc:len() do
+        print("msg.desc[" .. i .. "] = " .. msg.desc:get(i))
     end
+
+    local buffer = pb.serializeToString(msg)
+    local msg2 = pb.new("net.tb_Person")
+    pb.parseFromString(msg2, buffer)
+
+    print("pb_cpp_test pass [" ..  buffer .. "]")
 end
 
-function pbtest3() 
+function pb_table_test() 
     local message = {
         number = "13615632545",
         email = "13615632545@163.com",
@@ -71,10 +77,45 @@ function pbtest3()
     assert(msg.desc[1] == "first")
     assert(msg.desc[2] == "second")
     assert(msg.desc[3] == "three")
-
-    print("pbtest3 " .. buffer)
+    print("pb_table_test pass [" ..  buffer .. "]")
 end
 
-pbtest()
-pbtest3()
+function pb_cpp_test_million()
+    for i=1, 1000000 do
+        local msg = pb.new("net.tb_Person")
+        msg.number = "13615632545"
+        msg.email = "13615632545@163.com"
+        msg.age = 28
+        msg.ptype = 2
+        msg.desc:add("first")
+        msg.desc:add("second")
+        msg.desc:add("three")
 
+        local buffer = pb.serializeToString(msg)
+        local msg2 = pb.new("net.tb_Person")
+        pb.parseFromString(msg2, buffer)
+    end
+    print("pb_cpp_test_million pass")
+end
+
+function pb_table_test_million() 
+    for i=1, 1000000 do
+        local message = {
+            number = "13615632545",
+            email = "13615632545@163.com",
+            age = 28,
+            ptype = "WORK",
+            desc = {"first", "second", "three"}
+        }
+   
+        local buffer = pb.encode("net.tb_Person", message)
+        local msg = pb.decode("net.tb_Person", buffer)
+    end
+    print("pb_table_test_million pass")
+end
+
+pb_cpp_test()
+pb_table_test()
+
+pb_cpp_test_million()
+pb_table_test_million()
