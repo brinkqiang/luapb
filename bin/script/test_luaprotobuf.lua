1c97bbff8e8d9ba71211a39128af237958bdcbf8
brinkqiang <brink.qiang@gmail.com>
2019-09-06 22:54:24 +0800

    add

--- bin/script/test_luaprotobuf.lua
+++ bin/script/test_luaprotobuf.lua
@@ -2,7 +2,7 @@
 local pb = require "luapb"
 pb.import("test_luaprotobuf.proto")
 
-function test_luapb_encode()
+function test_luapb_encode(num)
     local data = {
         contacts = {
             {phonenumber = 123456789, name = "foo1"},
@@ -20,13 +20,13 @@ function test_luapb_encode()
     local buffer = pb.serializeToString(msg)
 
     local t1 = os.clock();
-    for i=1,1000000 do
+    for i=1,num do
         local buf = pb.serializeToString(msg)
     end 
-    print("num="..num.."\ttime="..os.clock()-t1)
+    print("num=".. num .."\ttime="..os.clock()-t1)
 end 
 
-function test_luapb_decode()
+function test_luapb_decode(num)
     local data = {
         contacts = {
             {phonenumber = 123456789, name = "foo1"},
@@ -44,14 +44,14 @@ function test_luapb_decode()
     local buffer = pb.serializeToString(msg)
 
     local t1 = os.clock();
-    for i=1,1000000 do
+    for i=1,num do
         local buf = pb.parseFromString(msg)
     end 
-    print("num="..num.."\ttime="..os.clock()-t1)
+    print("num=".. num .."\ttime="..os.clock()-t1)
 end 
 
-test_luapb_encode()
-test_luapb_decode()
+test_luapb_encode(1000000)
+test_luapb_decode(1000000)
 
 --testn(1000000,pb.encode,"Person",data)
 --testn(1000000,pb.decode,"Person",chunk)
