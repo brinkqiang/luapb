58a78ef456ab394f5638cf2cfeb11fcad5009185
brinkqiang <brink.qiang@gmail.com>
2019-09-06 23:57:41 +0800

    add

---
+++ bin/script/test_luapb.lua
@@ -0,0 +1,58 @@
+
+local pb = require "luapb"
+pb.import("test_luaprotobuf.proto")
+
+function test_luapb_encode(num)
+    local data = {
+        contacts = {
+            {phonenumber = 123456789, name = "foo1"},
+        } , 
+        name = "hello",age=18,address="12345你好啊"
+    }
+    local loop = 80
+    for i=1,loop do 
+        table.insert(data.contacts,{phonenumber = 123456789, name = "foo"..(i+1)})
+    end 
+    
+    local chunk = pb.encode("Person",data)
+    local msg = pb.new("Person")
+    pb.parseFromString(msg, chunk)
+    local buffer = pb.serializeToString(msg)
+
+    local t1 = os.clock();
+    for i=1,num do
+        local buf = pb.serializeToString(msg)
+    end 
+    print("num=".. num .."\ttime="..os.clock()-t1)
+end 
+
+function test_luapb_decode(num)
+    local data = {
+        contacts = {
+            {phonenumber = 123456789, name = "foo1"},
+        } , 
+        name = "hello",age=18,address="12345你好啊"
+    }
+    local loop = 80
+    for i=1,loop do 
+        table.insert(data.contacts,{phonenumber = 123456789, name = "foo"..(i+1)})
+    end 
+    
+    local chunk = pb.encode("Person", data)
+    local msg = pb.new("Person")
+    pb.parseFromString(msg, chunk)
+    local buffer = pb.serializeToString(msg)
+
+    local t1 = os.clock();
+    for i=1,num do
+        pb.parseFromString(msg, chunk)
+    end 
+    print("num=".. num .."\ttime="..os.clock()-t1)
+end 
+
+test_luapb_encode(1000000)
+test_luapb_decode(1000000)
+
+--testn(1000000,pb.encode,"Person",data)
+--testn(1000000,pb.decode,"Person",chunk)
+
--- bin/script/test_luaprotobuf.lua
+++ bin/script/test_luaprotobuf.lua
@@ -1,58 +1,54 @@
+local protoc = require "protoc"
+local pb = require "pb"
+local buffer = require "pb.buffer"
+protoc:new():loadfile("test.proto")
 
-local pb = require "luapb"
-pb.import("test_luaprotobuf.proto")
-
-function test_luapb_encode(num)
+function test_encode(num)
+    local de = pb.decode("Person", chunk)
     local data = {
-        contacts = {
-            {phonenumber = 123456789, name = "foo1"},
-        } , 
-        name = "hello",age=18,address="12345你好啊"
+        contacts = {{phonenumber = 123456789, name = "foo1"}},
+        name = "hello",
+        age = 18,
+        address = "12345你好啊"
     }
     local loop = 80
-    for i=1,loop do 
-        table.insert(data.contacts,{phonenumber = 123456789, name = "foo"..(i+1)})
-    end 
-    
-    local chunk = pb.encode("Person",data)
-    local msg = pb.new("Person")
-    pb.parseFromString(msg, chunk)
-    local buffer = pb.serializeToString(msg)
+    for i = 1, loop do
+        table.insert(data.contacts,
+                     {phonenumber = 123456789, name = "foo" .. (i + 1)})
+    end
+    local b = buffer.new()
+    local chunk = pb.encode("Person", data, b)
+    -- print(string.len(chunk))
+    local de = pb.decode("Person", chunk)
+    local t1 = os.clock()
+    for i = 1, num do local msg = pb.encode("Person", data) end
+    print("num=" .. num .. "\ttime=" .. os.clock() - t1)
+end
 
-    local t1 = os.clock();
-    for i=1,num do
-        local buf = pb.serializeToString(msg)
-    end 
-    print("num=".. num .."\ttime="..os.clock()-t1)
-end 
-
-function test_luapb_decode(num)
+function test_decode(num)
     local data = {
-        contacts = {
-            {phonenumber = 123456789, name = "foo1"},
-        } , 
-        name = "hello",age=18,address="12345你好啊"
+        contacts = {{phonenumber = 123456789, name = "foo1"}},
+        name = "hello",
+        age = 18,
+        address = "12345你好啊"
     }
     local loop = 80
-    for i=1,loop do 
-        table.insert(data.contacts,{phonenumber = 123456789, name = "foo"..(i+1)})
-    end 
-    
-    local chunk = pb.encode("Person", data)
-    local msg = pb.new("Person")
-    pb.parseFromString(msg, chunk)
-    local buffer = pb.serializeToString(msg)
+    for i = 1, loop do
+        table.insert(data.contacts,
+                     {phonenumber = 123456789, name = "foo" .. (i + 1)})
+    end
+    local b = buffer.new()
+    local chunk = pb.encode("Person", data, b)
+    -- print(string.len(chunk))
+    local de = pb.decode("Person", chunk)
 
-    local t1 = os.clock();
-    for i=1,num do
-        pb.parseFromString(msg, chunk)
-    end 
-    print("num=".. num .."\ttime="..os.clock()-t1)
-end 
+    local t1 = os.clock()
+    for i = 1, num do local buf = pb.decode("Person", chunk) end
+    print("num=" .. num .. "\ttime=" .. os.clock() - t1)
+end
 
-test_luapb_encode(1000000)
-test_luapb_decode(1000000)
+test_encode(1000000)
 
---testn(1000000,pb.encode,"Person",data)
---testn(1000000,pb.decode,"Person",chunk)
+-- print(#chunk)
+test_decode(1000000)
 
