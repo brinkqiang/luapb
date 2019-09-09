f14ff1df93c42b89050da1708b22adcf22047b5d
brinkqiang <brink.qiang@gmail.com>
2019-09-09 09:56:19 +0800

    add

--- src/lua/luaconf.h
+++ src/lua/luaconf.h
@@ -192,8 +192,7 @@
         LUA_LDIR"?.lua;"  LUA_LDIR"?\\init.lua;" \
         LUA_CDIR"?.lua;"  LUA_CDIR"?\\init.lua;" \
         LUA_SHRDIR"?.lua;" LUA_SHRDIR"?\\init.lua;" \
-        ".\\?.lua;" ".\\?\\init.lua;" "!\\..\\?.lua;" \
-        ".\\..\\script\\?.lua"
+        ".\\?.lua;" ".\\?\\init.lua;" "!\\..\\?.lua;" ".\\..\\script\\?.lua" \
 
 #define LUA_CPATH_DEFAULT \
         LUA_CDIR"?.dll;" \
