d627f6b38be8e085ba73ec59494c169ecb0c5bec
brinkqiang <brink.qiang@gmail.com>
2022-01-07 17:23:36 +0800

    add

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ rmdir /S /Q build
 mkdir build
 pushd build
 cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-cmake --build . --config debug
+cmake --build . --config debug -- /m:%NUMBER_OF_PROCESSORS%
 popd
 
 rem pause
